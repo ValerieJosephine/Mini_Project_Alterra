b0c86171686fe597b5705f9b0c502ce900bf6705
ValerieJosephine <salindehoadeline@gmail.com>
2022-05-01 21:58:41 +0700

    menambahkan coding

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func Init() *echo.Echo {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Selamat datang dalam sistem warehouse!")
 	})
 
 	return e
