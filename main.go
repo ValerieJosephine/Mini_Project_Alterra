d50fdfd37adf391f16cf4b30d3301ec441dbb4d5
ValerieJosephine <salindehoadeline@gmail.com>
2022-05-01 20:27:08 +0700

    menambahkan routes

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
-)
-
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8000"))
+	e := routes.routes()
+
+	e.Logger.Fatal(e.start(":8000"))
 }
